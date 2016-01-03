858872f1b88222770f69fe0e751e0056b218f57d
willtuna <willvegapunk@gmail.com>
2016-01-03 20:35:49 +0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,4 +1,4 @@
- 
+
 #ifndef _Graph_H_
 #define _Graph_H_
 
@@ -13,17 +13,31 @@ using namespace std;
 
 struct Edge{
 	int FirstNode,SecondNode,weight;
+	Edge(int f=0, int t=0, int w=0):FirstNode(f),SecondNode(t),weight(w)// ctor
+	{}
 };
 
-class comp//  compare class for std:priority_queue<, ,compare>    
+struct heapedge{
+	int from, to, weight;
+
+	heapedge(int f=0, int t=0, int w=0):from(f),to(t),weight(w)// ctor
+	{}
+
+};
+
+
+//  preprocessing has order    we need to declare heapedge first
+class comp//  compare class for std:priority_queue<, ,compare>
 //since priority_queue_is MAXHEAP by default
 {
 public:
-	bool operator()(const AdjNode & a,const AdjNode & b)const
+	bool operator()(const heapedge & a,const heapedge & b)const
 	{
 		return a.weight > b.weight;
 	}
-}
+};
+
+
 
 struct AdjNode{
 	int index,weight;
